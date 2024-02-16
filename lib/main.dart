e31ee3ff724f1319f1c940a409cb994eaccd7005
Giles <giles112000@gmail.com>
2024-02-17 00:05:45 +0800

    Add portrait mode lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'router/router.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  /// Lock portrait mode.
+  unawaited(SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]));
+
   runApp(
     const ProviderScope(
       child: WeatherApp(),
